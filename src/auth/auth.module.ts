661abd2e80a871028d8c00a6036ba0df99233c3a
Viqtor <viqto0r@gmail.com>
2025-01-18 14:52:01 +0300

    feat: подключены модели к mongoDB

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,19 @@
+import { AuthModel } from './auth.model';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: AuthModel,
+        schemaOptions: {
+          collection: 'Auth',
+        },
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,19 @@
+import { TopPageModel } from './top-page.model';
 import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-  controllers: [TopPageController]
+  controllers: [TopPageController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: TopPageModel,
+        schemaOptions: {
+          collection: 'TopPage',
+        },
+      },
+    ]),
+  ],
 })
 export class TopPageModule {}
